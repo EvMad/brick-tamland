8adb94f223929216e13f119a7e52e8742b244a1b
GitPit <rifflifter@gmail.com>
2021-06-15 16:51:28 -0400

    added js varaiables and started functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,28 @@ var city;
 
 var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+// search input
 
+fetch (queryURL, {
+    method: 'GET',
+    credentials: 'same-origin',
+    cache: 
+})
+
+
+// local storage
+
+let searches = [];
+
+const addCity = (event)=>{
+    event.preventDefault();
+    let search = {
+        id: Date.now(),
+        title: document.getElementById("#cityName").Value,
+    }
+
+    searches.push(search);
+    document.forms[0].reset();
+
+    localStorage.setItem("citySearches", JSON.stringify(searches));
+}
