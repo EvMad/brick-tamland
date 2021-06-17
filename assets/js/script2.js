a7e9ba23f3474c5a7c53ef942f67e47f44a4194d
GitPit <rifflifter@gmail.com>
2021-06-17 11:16:02 -0400

    added jquery script for new strategy

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,26 +1,4 @@
-let iconEl = $(".cityWeatherIconName");
-let cityNameEl = $(".cityName");
-let cityTempEl = $(".cityTemp");
-let cityHumidityEl = $(".cityHumidity");
-let cityWindSpeedEl = $(".cityWindSpeed");
 
-
-function cityWeather() {
-    $.ajax({
-        url: queryUrl,
-        method: 'GET'
-    })
-        .then(function(weatherData){
-            let weatherObj = {
-                cityName: weatherDate.name,
-                cityTemp: weatherData.main.temp,
-                cityHumidity: weatherData.main.humidity,
-                cityWindSpeed: weatherData.wind.speed,
-                cityUVIndex: weatherData.coord,
-                cityWeatherIconName: weatherData.weather[0].icon
-            }
-        })
-        }
 
           // function getApi(queryURL) {
     //     fetch(URL)
